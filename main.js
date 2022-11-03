e06d094d9dad57e723d2d5769a9f3c210489fe2b
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2022-11-03 15:42:52 +0100

    Word Count

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+
 class WordCounter{
     constructor(text){
         this.text = text;
-        this.text.addEventListener('input',this.count)
+        this.text.addEventListener('input',() => {this.count()})
     }
     count(){
-        let wordStat = this.getWordStat(this.inputText.value.trim());
+        let wordStat = this.getWordStat(this.text.value.trim());
+        this.emitEvent(wordStat);
     }
     getWordStat(str) {
         let matches = str.match(/\S+/g);
@@ -23,6 +25,17 @@ class WordCounter{
             }
         });
         // dispatch the count event
-        this.inputText.dispatchEvent(countEvent);
+        this.text.dispatchEvent(countEvent);
+    }
 }
+const inputText = document.querySelector('#text');
+const statElem = document.querySelector('#stat');
+
+new WordCounter(inputText);
+
+const render = (event) => {
+    statElem.innerHTML = `<p>You've written <span class="highlight">${event.detail.wordStat.words} words</span> 
+    and <span class="highlight">${event.detail.wordStat.characters} characters</span>.</p>`;
 }
+
+inputText.addEventListener('count', render);
